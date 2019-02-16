a254840a2b1e9794fa18ead25924e78d592a9d97
Peter Smith <mail@petersmith.org>
2019-02-17 12:03:52 +1300

    Local changes

--- custom-settings.el
+++ custom-settings.el
@@ -4,26 +4,23 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(TeX-view-program-selection
-	 (quote
-		(((output-dvi has-no-display-manager)
-			"dvi2tty")
-		 ((output-dvi style-pstricks)
-			"dvips and gv")
-		 (output-dvi "xdvi")
-		 (output-pdf "xpdf")
-		 (output-html "xdg-open"))))
+   (quote
+    (((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "xpdf")
+     (output-html "xdg-open"))))
  '(calendar-date-style (quote european))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
-	 (quote
-		("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(org-agenda-files
-	 (quote
-		("/usr/home/psmith/NAS/Work/Research/PapersInProgress/KnowledgeProfessionsAI/K-PSFs-AI.org" "/home/psmith/NAS/Org/calendar.org" "/home/psmith/NAS/Org/diary.org" "/home/psmith/NAS/Org/flagged.org" "/home/psmith/NAS/Org/gtd.org" "/home/psmith/NAS/Org/journal.org" "/home/psmith/NAS/Org/refile.org" "/home/psmith/NAS/Org/rssFeeds.org")))
  '(package-selected-packages
-	 (quote
-		(go-mode elfeed-org magit notmuch org-pdfview pdf-tools writeroom-mode auctex color-theme-sanityinc-tomorrow zenburn-theme markdown-mode use-package org-plus-contrib)))
+   (quote
+    (ox-reveal color-theme solarized-theme go-mode elfeed-org magit org-pdfview pdf-tools writeroom-mode auctex color-theme-sanityinc-tomorrow zenburn-theme markdown-mode use-package org-plus-contrib)))
  '(preview-TeX-style-dir "/home/psmith/.emacs.d/elpa/auctex-11.89.7/latex" t)
  '(reftex-use-multiple-selection-buffers t)
  '(send-mail-function (quote smtpmail-send-it)))
--- init.el
+++ init.el
@@ -28,18 +28,37 @@
   (package-install 'org-plus-contrib))
 
 (eval-when-compile
-  (require 'use-package))
+(require 'use-package))
 (require 'diminish)
 (require 'bind-key)
 
 ;; load up all literate org-mode files in this directory
-(require 'org)
+;(require 'org)
+;
+;
+;(org-babel-load-file (concat user-emacs-directory "psmith.org"))
 
+(let* ((org (expand-file-name "psmith.org" user-emacs-directory))
+       (el  (expand-file-name "psmith.el" user-emacs-directory))
+       (elc (concat el "c")))
+  (when (and (file-exists-p elc)
+             (file-newer-than-file-p org elc))
+    (message "Byte compiled init is old - deleting...")
+    (dolist (file (directory-files user-emacs-directory nil "\\.elc$"))
+      (delete-file (expand-file-name file user-emacs-directory))))
 
-(org-babel-load-file (concat user-emacs-directory "psmith.org"))
+  (cond ((file-exists-p elc) (load elc nil t))
+        (t
+         (when (file-newer-than-file-p org el)
+           (message "Tangling the literate config...")
+           (unless (call-process "emacs" nil nil nil "--batch" "-l" "ob-tangle" "--eval"
+                                 (format "(org-babel-tangle-file \"%s\" \"%s\" \"emacs-lisp\")" org el))
+             (warn "There was a problem tangling the literate config")))
+
+         (load el nil t))))
 
 (setq gc-cons-threshold 800000)
 
 
 ;;; init.el ends here
-(put 'downcase-region 'disabled nil)
+
